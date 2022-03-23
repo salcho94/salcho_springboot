90a10a44da5acb9601d3d3e3c9a88a1d60f8e39a
salcho94 <salcho94@naver.com>
2022-03-23 16:14:54 +0900

    Initial commit

---
+++ src/main/java/com/salcho/salcho/SalchoApplication.java
@@ -0,0 +1,13 @@
+package com.salcho.salcho;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalchoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalchoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/salcho/salcho/SalchoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salcho.salcho;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalchoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
